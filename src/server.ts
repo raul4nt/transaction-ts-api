f2c7e741996baaf0767c87d7c964d74f7ac81584
Raul Silveira <raulantsilveira@gmail.com>
2024-10-21 01:06:17 -0300

    config host 0.0.0.0

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,19 @@
 import { app } from './app'
 import { env } from './env'
 
+// app
+//   .listen({
+//     port: env.PORT,
+//   })
+//   .then(() => {
+//     console.log('HTTP Server Running')
+//   })
+
 app
   .listen({
     port: env.PORT,
+    host: '0.0.0.0',  // Adicione o host aqui
   })
   .then(() => {
-    console.log('HTTP Server Running')
-  })
+    console.log(`HTTP Server Running on port ${env.PORT}`);
+  });
